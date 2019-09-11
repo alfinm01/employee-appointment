5e1d244caf9cfd0eaf1a067f094a1bb65c3ded14
Dwi Cahyo Pangestu <dcpangestu@mail.com>
2019-09-11 10:57:58 +0700

    nambahin nodemon

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const start = async () => {
     try {
     	// Isi PORT di file .env (default: 3000)
         const server = await app.listen(process.env.PORT);
-		console.info(`Server listening on ${server.address().port}`);
+        console.info(`Server listening on ${server.address().port}`); 
     } catch (err) {
         console.error(err);
     }

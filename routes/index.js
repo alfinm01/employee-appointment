5e4a85f35ad39c427b4549e19ce212b0f3fbebc6
alfinm01 <arrestral@gmail.com>
2019-10-17 05:08:59 +0700

    CRUD test

--- routes/index.js
+++ routes/index.js
@@ -9,25 +9,82 @@ const router = express.Router();
 const path = require('path');
 const db = require('../datastore/config');
 
+/* CRUD test */
+
 /**
- * Users getter route
+ * Get all users
  */
 router.get('/users', (req, res) => {
 	db.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
 	    if (error) {
-	      throw error;
+	    	throw error;
 	    }
 	    res.status(200).json(results.rows);
 	 });
 });
 
 /**
- * Time logger in console
+ * Get user by id
  */
-router.use(function timeLog(req, res, next) {
-	console.log('Time: ', Date.now());
-	next();
+router.get('/user/:id', (req, res) => {
+	const id = parseInt(req.params.id);
+
+	db.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+	    if (error) {
+	    	throw error;
+	    }
+	    res.status(200).json(results.rows);
+	});
 });
+
+/**
+ * Create user
+ */
+router.post('/user', (req, res) => {
+	const { name, email } = req.body;
+
+	db.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+	    if (error) {
+	    	throw error;
+	    }
+	    res.status(201).send(`User added with ID: ${result.insertId}`);
+	});
+});
+
+/**
+ * Update user by id
+ */
+router.put('/user/:id', (req, res) => {
+	const id = parseInt(req.params.id);
+	const { name, email } = req.body;
+
+	db.query(
+	    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+	    [name, email, id],
+	    (error, results) => {
+		    if (error) {
+		    	throw error;
+		    }
+		    res.status(200).send(`User modified with ID: ${id}`);
+	    }
+	)
+});
+
+/**
+ * Delete user by id
+ */
+router.delete('/user/:id', (req, res) => {
+	const id = parseInt(req.params.id);
+
+	pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+	    if (error) {
+	    	throw error;
+	    }
+	    res.status(200).send(`User deleted with ID: ${id}`);
+	});
+});
+
+/* Web views */
 
 /**
  * Home page route
